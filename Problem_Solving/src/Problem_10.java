b46d468222818e8f6f0de90ba9ebea791095348a
Ayush Bansal <bayush611@gmail.com>
2021-03-30 19:26:13 +0530

    Birthday Cake Candles

--- Problem_Solving/src/Problem_10.java
+++ Problem_Solving/src/Problem_10.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Problem_10 {
+import java.util.*;
+
+public class Problem_10 {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int maxValue = 0;
+        int count =0;
+
+        int candles[] = new int[n];
+        for(int i =0;i<n;i++){
+            candles[i] = sc.nextInt();
+        }
+
+        for(int i =0;i<n;i++) {
+            if (candles[i] >= maxValue) {
+                maxValue = candles[i];
+            }
+        }
+            for (int ele:candles) {
+                if (ele == maxValue) {
+                    count++;
+                }
+            }
+
+        System.out.println(count);
+    }
 }
