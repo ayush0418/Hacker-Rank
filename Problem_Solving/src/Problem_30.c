aaa5efa0448e1d9df9185172e619c099a4b71c67
Ayush Bansal <bayush611@gmail.com>
2021-06-13 20:16:23 +0530

    'Maximum Element'(Some cases are not running)

--- Problem_Solving/src/Problem_30.c
+++ Problem_Solving/src/Problem_30.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 struct Node{
     int data;
@@ -7,23 +7,23 @@ struct Node{
 };
 
 int isEmpty(struct Node *top){
-    if(top == NULL){
+    if (top == NULL){
         return 1;
     }
-    return 0; 
+    return 0;
 }
 
 int isFull(struct Node *top){
-    struct Node *p = (struct Node*)malloc(sizeof(struct Node));
-    if(p == NULL){
+    struct Node *p = (struct Node *)malloc(sizeof(struct Node));
+    if (p == NULL){
         return 1;
     }
-    return 0; 
+    return 0;
 }
 
-struct Node* push(struct Node *top, int x){
-    if(!isFull(top)){
-        struct Node *n = (struct Node*)malloc(sizeof(struct Node));
+struct Node *push(struct Node *top, int x){
+    if (!isFull(top)){
+        struct Node *n = (struct Node *)malloc(sizeof(struct Node));
         n->data = x;
         n->next = top;
         top = n;
@@ -32,8 +32,8 @@ struct Node* push(struct Node *top, int x){
     return 0;
 }
 
-void pop(struct Node** top){
-    if(!isEmpty(*top)){
+void pop(struct Node **top){
+    if (!isEmpty(*top)){
         struct Node *n = *top;
         *top = (*top)->next;
         free(n);
@@ -41,30 +41,30 @@ void pop(struct Node** top){
 }
 
 void stackTop(struct Node *ptr){
-   long int new = ptr->data;
-    printf("%ld\n",new);
+    long int new = ptr->data;
+    printf("%ld\n", new);
 }
 
 int main(){
     struct Node *top = NULL;
 
     int n;
-  scanf("%d",&n);
-  int x;
-  long int val;
+    scanf("%d", &n);
+    int x;
+    long int val;
 
-  for(int i=0;i<n;i++){
-      scanf("%d",&x);
-      if (x == 1){
-        scanf("%ld",&val);
-        top = push(top,val);
-      }
-      if(x == 2){
-          pop(&top);
-      }
-      if(x == 3){
-          stackTop(top);
-      }
-  }
-  return 0;
+    for (int i = 0; i < n; i++){
+        scanf("%d", &x);
+        if (x == 1){
+            scanf("%ld", &val);
+            top = push(top, val);
+        }
+        if (x == 2){
+            pop(&top);
+        }
+        if (x == 3){
+            stackTop(top);
+        }
+    }
+    return 0;
 }
