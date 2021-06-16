e8de2b285b27c85f6528aa560844a2964752b9a5
Ayush Bansal <bayush611@gmail.com>
2021-06-16 16:38:13 +0530

    Simple Text Editor(ONE CASE REMAINING)

---
+++ Problem_Solving/src/Problem_33.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+int isEmpty(struct Node *top){
+    if (top == NULL){ return 1; }
+    return 0;
+}
+
+int isFull(struct Node *top){
+    struct Node *p = (struct Node *)malloc(sizeof(struct Node));
+    if (p == NULL){ return 1; }
+    return 0;
+}
+
+struct Node *push(struct Node *top, int x){
+    if (!isFull(top)){
+        struct Node *n = (struct Node *)malloc(sizeof(struct Node));
+        n->data = x;
+        n->next = top;
+        top = n;
+        return top;
+    }
+    return 0;
+}
+
+char pop(struct Node **top){
+    if (!isEmpty(*top)){
+        struct Node *n = *top;
+        *top = (*top)->next;
+        free(n);
+        return top;
+    }
+}
+
+int peek(struct Node *top, int x){
+    struct Node *n = top;
+    int e = 0;;
+    while(n!=NULL){
+        n= n->next;
+        e++;
+    }
+    for (int i = 0; (i < (e  -x) && top != NULL); i++){ top = top->next; }
+    if (top != NULL){ return top->data; }
+    else{ return -1; }
+}
+
+void StackLinkedListTraversal(struct Node *ptr){
+    printf("Stack: ");
+    char q;
+    while (ptr != NULL){
+        q = ptr->data;
+        printf("%c ", q);
+        ptr = ptr->next;
+    }
+}
+
+int main(){
+    struct Node *top = NULL;
+    int n, opr;
+    char ch;
+    printf("\nEnter Queries: ");
+    scanf("%d", &n);
+
+    while (n--) {
+        printf("\nEnter Operations: ");
+        scanf("%d", &opr);
+
+        if (opr == 1) {
+            char W[1000];
+            printf("\nEnter String(Opr 1): ");
+            scanf("%s", &W);
+            for (int i = 0; W[i] != '\0'; i++) {
+                top = push(top, W[i]);
+            }
+            StackLinkedListTraversal(top);
+        }
+
+        else if (opr == 2){
+            int k;
+            printf("\nEnter deleting number(Opr 2): ");
+            scanf("%d", &k);
+            for (int i = 0; i < k; i++) {
+                pop(&top);
+            }
+            StackLinkedListTraversal(top);
+        }
+        
+        else if (opr == 3){
+            int k;
+            char peek_ch;
+            printf("\nEnter peek number(Opr 3): ");
+            scanf("%d", &k);
+            peek_ch = peek(top, k);
+            printf("%c",peek_ch);
+        }
+        StackLinkedListTraversal(top);
+    }
+    return 0;
+}
