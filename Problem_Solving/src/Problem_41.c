5b7490b5dc4f6a2c5b53b1f92e693026056249af
Ayush Bansal <bayush611@gmail.com>
2021-06-19 15:43:22 +0530

    Insertion Sort - Part 2

---
+++ Problem_Solving/src/Problem_41.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void printArray(int* A, int n){
+    for (int i = 0; i < n; i++){ printf("%d ", A[i]); }
+    printf("\n");
+}
+
+void insertionSort(int *A, int n){
+    int key, j;
+    for (int i = 1; i <= n-1; i++){
+        key = A[i];
+        j = i-1;
+        while(j >= 0 && A[j] > key){
+            A[j+1] = A[j];
+            j--;
+        }
+        A[j+1] = key;
+        printArray(A, n);
+    }
+}
+
+int main(){ 
+    int n;
+    scanf("%d",&n);
+    int A[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&A[i]);
+    }
+    insertionSort(A, n);
+    return 0;
+}
