db7a3ed89283f56cab6eb54d5116fc5b193b3791
Ayush Bansal <bayush611@gmail.com>
2021-06-24 12:40:46 +0530

    Service Lane

---
+++ Problem_Solving/src/Problem_50.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+    int n, t;
+    scanf("%d %d",&n,&t);
+    
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+
+    while (t--){
+        int entry,exit;
+        scanf("%d %d", &entry,&exit);
+
+        int minimum = a[entry];
+        for(int i = entry; i <= exit; i++){
+            if(a[i]<minimum){
+                minimum = a[i];
+            }
+        }
+        printf("%d\n",minimum);      
+    }
+}
+    
