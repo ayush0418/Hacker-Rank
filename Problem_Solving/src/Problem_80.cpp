9851686b3084940d4a983c3d0926e5cc4296a830
Ayush Bansal <bayush611@gmail.com>
2021-07-13 14:20:05 +0530

    Beautiful Triplets

---
+++ Problem_Solving/src/Problem_80.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,d;
+    cin>> n >> d;
+    int a[n];
+
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    sort(a,a+n);
+
+    int count = 0;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(a[j] - a[i] > d){
+                break;
+            }
+            for(int k=j+1;k<n;k++){
+                if(a[j] - a[i] == d && a[k] - a[j] == d){
+                    count++;
+                }
+            }
+        }
+    }
+    cout<<count<<endl;
+}
