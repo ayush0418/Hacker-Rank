beddb0c5e0087d2f2bc4d291fb6b6e2c4da3670e
Ayush Bansal <bayush611@gmail.com>
2021-07-31 11:36:56 +0530

    Counting Sort 1

---
+++ Problem_Solving/src/Program_117.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    
+
+    int b[100] = {0};
+    for(int i=0;i<n;i++){
+        b[a[i]]++;
+    }
+
+    for(int i=0;i<100;i++){
+        cout<<b[i]<<" ";
+    }
+
+}
